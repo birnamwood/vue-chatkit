ee3d6e383054a7b1afe4b5801440900e418e523a
birnamwood <birnamwood19@gmail.com>
2019-12-14 15:38:17 +0900

    ChatNavBarの作成

---
+++ src/components/ChatNavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar id="chat-navbar" toggleable="md" type="dark" variant="info">
+    <b-navbar-brand href="#">
+      Vue chat
+    </b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-navbar-text>{{ user.name }} | </b-navbar-text>
+      <b-nav-item href="#" active>Logout</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'ChatNavbar',
+  computed: {
+    ...mapState([
+      'user',
+    ])
+  },
+}
+</script>
+
+<style scoped>
+  #chat-navbar {
+    margin-bottom: 15px;
+  }
+</style>
